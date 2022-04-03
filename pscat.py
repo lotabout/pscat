22032534ce8bc0a80883d1ce5177b084e23c8027
Jinzhou Zhang <lotabout@gmail.com>
2022-04-03 15:26:00 +0800

    add reader/writer wrapper for file/stdin/stdout/socket file objects

--- pscat.py
+++ pscat.py
@@ -14,6 +14,88 @@ logging.basicConfig(filename='pscat.log', encoding='utf-8', level=logging.DEBUG)
 
 Socket = namedtuple('Socket', ['rfd', 'wfd'])
 
+import io
+class ReaderWrapper(object):
+    def __init__(self, io):
+        super(ReaderWrapper, self).__init__()
+        self.io = io
+        self.eof = False
+
+    def next_batches(self, batch_size):
+        """non-blocking fds would consume as much as we can by batches.
+
+            this function assumes fd's ready for reading.
+        """
+
+        if self.eof:
+            return None
+
+        if isinstance(self.io, io.TextIOWrapper):
+            if self.io.isatty():
+                return self._read_stdin(batch_size)
+            else:
+                return self._read_file_io(batch_size)
+        elif isinstance(self.io, socket.socket):
+            return self._read_socket(batch_size)
+
+    def eof_met(self):
+        return self.eof
+
+    def _read_stdin(self, batch_size):
+        # for stdin, we could only fetch one batch, cause there is no safe way
+        # to know whether there is more data without blocking
+
+        # stdin should use raw for reading
+        bytes = self.io.buffer.raw.read(batch_size)
+        if len(bytes) == 0:
+            log.debug('ala')
+            self.eof = True
+        else:
+            log.debug('asdf')
+            yield bytes
+
+    def _read_file_io(self, batch_size):
+        # File IO could be read till the end
+        bytes = self.io.read(batch_size)
+        while len(bytes) > 0:
+            yield bytes
+            bytes = self.io.read(batch_size)
+        self.eof = True
+
+    def _read_socket(self, batch_size):
+        bytes = self.io.recv(batch_size)
+        while len(bytes) >= batch_size and not self.io.getblocking():
+            # enters only in non-blocking mode
+            yield bytes
+            try:
+                bytes = self.io.recv(batch_size)
+            except BlockingIOError:
+                # no more data, just returns
+                break
+        else:
+            # last bits of bytes
+            if len(bytes) == 0:
+                self.eof = True
+            else:
+                yield bytes
+
+    def close(self):
+        self.io.close()
+
+class WriterWrapper(object):
+    def __init__(self, io):
+        super(WriterWrapper, self).__init__()
+        self.io = io
+    def write(self, bytes):
+        if isinstance(self.io, io.TextIOWrapper):
+            self.io.buffer.raw.write(bytes)
+        elif isinstance(self.io, io.IOBase):
+            self.io.write(bytes)
+        elif isinstance(self.io, socket.socket):
+            self.io.sendall(bytes)
+    def close(self):
+        self.io.close()
+
 sel = selectors.DefaultSelector()
 
 def usage():
@@ -25,32 +107,30 @@ def parse_args():
     parser.add_argument('addr2', type=str, help='The first address')
     return parser.parse_args()
 
-def set_input_nonblocking():
-    import fcntl
-    import os
-    orig_fl = fcntl.fcntl(sys.stdin, fcntl.F_GETFL)
-    fcntl.fcntl(sys.stdin, fcntl.F_SETFL, orig_fl | os.O_NONBLOCK)
-
 def pscat_open(address):
     log.info(f'opening address {address}')
 
     if address == '-':
-        set_input_nonblocking()
         return Socket(rfd = sys.stdin, wfd = sys.stdout)
     else:
         raise Exception(f"address type not supported: {address}")
 
-read_buf = bytearray(1024)
-def copy_data(src_fd, dst_fd, mask):
-    log.debug(f'copy data from {src_fd} -> {dst_fd}')
-    bytes_read = src_fd.buffer.readinto(read_buf)
-    if not bytes_read:
-        log.debug('EOF MET')
-        return True # means EOF
-    log.debug(f'copy data: {read_buf[:bytes_read]}')
-    dst_fd.buffer.write(read_buf[:bytes_read])
-    dst_fd.buffer.flush()
-    return False
+class Pipe(object):
+    def __init__(self, rfd, wfd_fd):
+        super(Pipe, self).__init__()
+        self.input = ReaderWrapper(rfd)
+        self.output = WriterWrapper(wfd_fd)
+
+    def transmit(self) -> bool:
+        # returns true on EOF
+        for batch in self.input.next_batches(4096):
+            log.debug(f'TRX batch: {batch}')
+            self.output.write(batch)
+        return self.input.eof_met()
+
+    def close(self):
+        self.input.close()
+        self.output.close()
 
 registerred_fds = set()
 def register_event(fileobj, event, data):
@@ -58,22 +138,27 @@ def register_event(fileobj, event, data):
         return
     sel.register(fileobj, event, data)
     registerred_fds.add(fileobj)
+def unregister(fileobj):
+    sel.unregister(fileobj)
+    registerred_fds.remove(fileobj)
 
 def pscat_connect(sock1, sock2):
     global registerred_fds
     if sock1.rfd is not None and sock2.wfd is not None:
-        register_event(sock1.rfd, selectors.EVENT_READ, (copy_data, sock2.wfd))
+        pipe = Pipe(sock1.rfd, sock2.wfd)
+        register_event(sock1.rfd, selectors.EVENT_READ, pipe)
 
     if sock2.rfd is not None and sock1.wfd is not None:
-        register_event(sock2.rfd, selectors.EVENT_READ, (copy_data, sock1.wfd))
+        pipe = Pipe(sock2.rfd, sock1.wfd)
+        register_event(sock2.rfd, selectors.EVENT_READ, pipe)
 
-    while True:
+    while len(sel.get_map()) > 0:
         events = sel.select()
         for key, mask in events:
-            (callback, dst_fd) = key.data
-            eof_met = callback(key.fileobj, dst_fd, mask)
-            if eof_met:
-                return
+            pipe = key.data
+            eof = pipe.transmit()
+            if eof:
+                unregister(key.fileobj)
 
 def pscat(args, address1, address2):
     sock1 = pscat_open(address1)
